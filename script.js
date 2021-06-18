dd2fc577656ecc787b29250c897ecb69e8d9b631
Hung Duong Quang <qhungg289@protonmail.com>
2021-06-18 20:55:09 +0700

    Add turn indicator and change player's name

--- script.js
+++ script.js
@@ -56,32 +56,125 @@ const GameController = ((arr, mark1, mark2) => {
 	let newMatchButton = document.getElementById("new-match-button");
 	let restartButton = document.getElementById("restart-button");
 	let gridItems = document.getElementsByClassName("grid-item");
+
 	let p1Score = document.getElementById("player1-score");
 	let p1Name = document.getElementById("player1-name");
+	let p1NameEditBtn = document.getElementById("change-name-1");
+	let p1NameEditModal = document.getElementById("change-name-1-modal");
+	let p1NameField = document.getElementById("name-field-1");
+	let p1NameSubmit = document.getElementById("submit-1");
+	let p1NameContainer = document.getElementById("name-container-1");
+
 	let p2Score = document.getElementById("player2-score");
 	let p2Name = document.getElementById("player2-name");
+	let p2NameEditBtn = document.getElementById("change-name-2");
+	let p2NameEditModal = document.getElementById("change-name-2-modal");
+	let p2NameField = document.getElementById("name-field-2");
+	let p2NameSubmit = document.getElementById("submit-2");
+	let p2NameContainer = document.getElementById("name-container-2");
 
 	p1Name.innerText = p1.getName();
 	p2Name.innerText = p2.getName();
 	p1Score.innerText = p1.getScore();
 	p2Score.innerText = p2.getScore();
 
+	window.onclick = (event) => {
+		if (
+			event.target == p1NameEditModal ||
+			event.target == p2NameEditModal
+		) {
+			p1NameEditModal.style.display = "none";
+			p2NameEditModal.style.display = "none";
+		}
+	};
+
+	p1NameSubmit.onclick = () => {
+		if (document.getElementById("name-field-1").value == "") {
+			alert("Don't leave this empty!");
+		} else {
+			p1.setName(`${document.getElementById("name-field-1").value}`);
+			p1Name.innerText = p1.getName();
+			document.getElementById("name-field-1").value = null;
+			p1NameEditModal.style.display = "none";
+		}
+	};
+
+	p2NameSubmit.onclick = () => {
+		if (document.getElementById("name-field-2").value == "") {
+			alert("Don't leave this empty!");
+		} else {
+			p2.setName(`${document.getElementById("name-field-2").value}`);
+			p2Name.innerText = p2.getName();
+			document.getElementById("name-field-2").value = null;
+			p2NameEditModal.style.display = "none";
+		}
+	};
+
+	p1NameField.addEventListener("keyup", (field) => {
+		if (field.key === "Enter") {
+			p1NameSubmit.click();
+		}
+	});
+
+	p2NameField.addEventListener("keyup", (field) => {
+		if (field.key === "Enter") {
+			p2NameSubmit.click();
+		}
+	});
+
+	newMatchButton.onclick = () => {
+		[...gridItems].forEach((e) => e.parentNode.removeChild(e));
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0; j < arr[i].length; j++) {
+				arr[i][j] = "";
+			}
+		}
+		move = 0;
+		p1.wonStatus = false;
+		p2.wonStatus = false;
+		resultBox.style.display = "none";
+		GameBoard.renderBoard(GameController.turnCheckAndMark);
+	};
+
+	p1NameEditBtn.onclick = () => {
+		p1NameEditModal.style.display = "block";
+	};
+
+	p2NameEditBtn.onclick = () => {
+		p2NameEditModal.style.display = "block";
+	};
+
+	restartButton.onclick = () => {
+		[...gridItems].forEach((e) => e.parentNode.removeChild(e));
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0; j < arr[i].length; j++) {
+				arr[i][j] = "";
+			}
+		}
+		move = 0;
+		p1.wonStatus = false;
+		p2.wonStatus = false;
+		GameBoard.renderBoard(GameController.turnCheckAndMark);
+	};
+
 	const turnCheckAndMark = (cell, index1, index2) => {
-		if (turn === true && cell.innerText === "") {
+		if (turn === true && cell.innerHTML === "") {
 			cell.innerHTML = `<i class="fas fa-times"></i>`;
 			cell.style.color = "red";
 			arr[index1][index2] = mark1;
 			checkWon();
 			winnerFound();
+			turnIndicator();
 			++move;
 			turn = false;
 			cell.disabled = true;
-		} else if (turn === false && cell.innerText === "") {
+		} else if (turn === false && cell.innerHTML === "") {
 			cell.innerHTML = `<i class="far fa-circle"></i>`;
 			cell.style.color = "blue";
 			arr[index1][index2] = mark2;
 			checkWon();
 			winnerFound();
+			turnIndicator();
 			++move;
 			turn = true;
 			cell.disabled = true;
@@ -187,20 +280,6 @@ const GameController = ((arr, mark1, mark2) => {
 		}
 	};
 
-	newMatchButton.onclick = () => {
-		[...gridItems].forEach((e) => e.parentNode.removeChild(e));
-		for (let i = 0; i < arr.length; i++) {
-			for (let j = 0; j < arr[i].length; j++) {
-				arr[i][j] = "";
-			}
-		}
-		move = 0;
-		p1.wonStatus = false;
-		p2.wonStatus = false;
-		resultBox.style.display = "none";
-		GameBoard.renderBoard(GameController.turnCheckAndMark);
-	};
-
 	const winnerFound = () => {
 		if (p1.wonStatus === true) {
 			resultBox.style.display = "block";
@@ -222,17 +301,14 @@ const GameController = ((arr, mark1, mark2) => {
 		}
 	};
 
-	restartButton.onclick = () => {
-		[...gridItems].forEach((e) => e.parentNode.removeChild(e));
-		for (let i = 0; i < arr.length; i++) {
-			for (let j = 0; j < arr[i].length; j++) {
-				arr[i][j] = "";
-			}
+	const turnIndicator = () => {
+		if (turn === false) {
+			p1NameContainer.style.borderBottom = "2px red solid";
+			p2NameContainer.style.borderBottom = "none";
+		} else {
+			p1NameContainer.style.borderBottom = "none";
+			p2NameContainer.style.borderBottom = "2px blue solid";
 		}
-		move = 0;
-		p1.wonStatus = false;
-		p2.wonStatus = false;
-		GameBoard.renderBoard(GameController.turnCheckAndMark);
 	};
 
 	return { turnCheckAndMark };
